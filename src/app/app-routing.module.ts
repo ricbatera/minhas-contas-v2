74f4571185d262d4cacde383055861fbdf54b37a
Ricardo Alves Roberto <contato@rdrtech.com.br>
2022-10-12 11:28:21 -0300

    novo projeto de contas v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SistemaComponent } from './sistema/sistema.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: SistemaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SistemaModule } from './sistema/sistema.module';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SistemaModule,
+
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue:'pt'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
